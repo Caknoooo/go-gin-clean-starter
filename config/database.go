874245c00e920600e0cd109d5ff83822430f613b
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-17 06:35:25 +0700

    feat: setup database connection

--- config/database.go
+++ config/database.go
@@ -1,1 +1,60 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"github.com/Caknoooo/golang-clean_template/entities"
+)
+
+func SetUpDatabaseConnection() *gorm.DB{
+	if os.Getenv("APP_ENV") != "Production"{
+		err := godotenv.Load(".env")
+		if err != nil{
+			fmt.Println(err)
+			panic(err)
+		}
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v TimeZone=Asia/Jakarta", dbHost, dbUser, dbPass, dbName, dbPort)
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: dsn,
+		// Menambahkan opsi berikut akan memungkinkan driver database
+		// untuk mendukung tipe data UUID secara bawaan.
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+
+	if err != nil{
+		fmt.Println(err)
+		panic(err)
+	}
+
+	if err := db.AutoMigrate(
+		entities.User{},
+	); err != nil{
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println("Database Connected")
+	return db
+}
+
+func ClosDatabaseConnection(db *gorm.DB){
+	dbSQL, err := db.DB()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	dbSQL.Close()
+}
