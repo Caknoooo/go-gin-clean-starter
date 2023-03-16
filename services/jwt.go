b7cb4d8b516d73acde39ec8766d4d723e9abb09f
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-17 06:33:24 +0700

    feat: add jwtservice and userservice

--- services/jwt.go
+++ services/jwt.go
@@ -1,1 +1,84 @@
 package services
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+)
+
+type JWTService interface {
+	GenerateToken(UserID uuid.UUID, role string) string
+	ValidateToken(token string) (*jwt.Token, error)
+	GetUserIDByToken(token string) (uuid.UUID, error)
+}
+
+type jwtCustomClaim struct {
+	UserID uuid.UUID `json:"user_id"`
+	Role   string    `json:"role"`
+	jwt.RegisteredClaims
+}
+
+type jwtService struct {
+	secretKey string
+	issuer    string
+}
+
+func NewJWTService() JWTService {
+	return &jwtService{
+		secretKey: getSecretKey(),
+		issuer:    "Template",
+	}
+}
+
+func getSecretKey() string {
+	secretKey := os.Getenv("JWT_SECRET")
+	if secretKey == "" {
+		secretKey = "Template"
+	}
+	return secretKey
+}
+
+func (j *jwtService) GenerateToken(UserID uuid.UUID, role string) string {
+	claims := jwtCustomClaim{
+		UserID,
+		role,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 120)),
+			Issuer: j.issuer,
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tx, err := token.SignedString([]byte(j.secretKey))
+	if err != nil{
+		log.Println(err)
+	}
+	return tx
+}
+
+func (j *jwtService) parseToken(t_ *jwt.Token)(any, error){
+	if _, ok := t_.Method.(*jwt.SigningMethodHMAC); !ok{
+		return nil, fmt.Errorf("unexpected signing method %v", t_.Header["alg"])
+	}
+	return []byte(j.secretKey), nil
+}
+
+func (j *jwtService) ValidateToken(token string)(*jwt.Token, error){
+	return jwt.Parse(token, j.parseToken);
+}
+
+func (j *jwtService) GetUserIDByToken(token string)(uuid.UUID, error){
+	t_Token, err := j.ValidateToken(token)
+	if err != nil{
+		return uuid.Nil, err
+	}
+	claims := t_Token.Claims.(jwt.MapClaims)
+	id := fmt.Sprintf("%v", claims["user_id"])
+	teamID, _ := uuid.Parse(id)
+	return teamID, nil
+}
--- services/user.go
+++ services/user.go
@@ -1,1 +1,96 @@
 package services
+
+import (
+	"context"
+
+	"github.com/Caknoooo/golang-clean_template/dto"
+	"github.com/Caknoooo/golang-clean_template/entities"
+	"github.com/Caknoooo/golang-clean_template/helpers"
+	"github.com/Caknoooo/golang-clean_template/repository"
+	"github.com/google/uuid"
+	"github.com/mashingan/smapping"
+)
+
+type UserService interface {
+	RegisterUser(ctx context.Context, userDTO dto.UserCreateDTO) (entities.User, error)
+	GetAllUser(ctx context.Context) ([]entities.User, error)
+	GetUserByID(ctx context.Context, userID uuid.UUID) (entities.User, error)
+	GetUserByEmail(ctx context.Context, email string) (entities.User, error)
+	CheckUser(ctx context.Context, email string) (bool, error)
+	UpdateUser(ctx context.Context, userDTO dto.UserUpdateDTO) error
+	DeleteUser(ctx context.Context, userID uuid.UUID) error
+	Verify(ctx context.Context, email string, password string) (bool, error)
+}
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserService(ur repository.UserRepository) UserService {
+	return &userService{
+		userRepository: ur,
+	}
+}
+
+func (us *userService) RegisterUser(ctx context.Context, userDTO dto.UserCreateDTO) (entities.User, error) {
+	user := entities.User{}
+	user.Role = "user"
+	err := smapping.FillStruct(&user, smapping.MapFields(userDTO))
+	if err != nil {
+		return user, err
+	}
+	return us.userRepository.RegisterUser(ctx, user)
+}
+
+func (us *userService) GetAllUser(ctx context.Context) ([]entities.User, error) {
+	return us.userRepository.GetAllUser(ctx)
+}
+
+func (us *userService) GetUserByID(ctx context.Context, userID uuid.UUID) (entities.User, error) {
+	return us.userRepository.GetUserByID(ctx, userID)
+}
+
+func (us *userService) GetUserByEmail(ctx context.Context, email string) (entities.User, error) {
+	return us.userRepository.GetUserByEmail(ctx, email)
+}
+
+func (us *userService) CheckUser(ctx context.Context, email string) (bool, error) {
+	res, err := us.userRepository.GetUserByEmail(ctx, email)
+	if err != nil {
+		return false, err
+	}
+
+	if res.Email == "" {
+		return false, err
+	}
+	return true, nil
+}
+
+func (us *userService) UpdateUser(ctx context.Context, userDTO dto.UserUpdateDTO) error {
+	user := entities.User{}
+	if err := smapping.FillStruct(&user, smapping.MapFields(userDTO)); err != nil {
+		return nil
+	}
+	return us.userRepository.UpdateUser(ctx, user)
+}
+
+func (us *userService) DeleteUser(ctx context.Context, userID uuid.UUID) error {
+	return us.userRepository.DeleteUser(ctx, userID)
+}
+
+func (us *userService) Verify(ctx context.Context, email string, password string) (bool, error) {
+	res, err := us.userRepository.GetUserByEmail(ctx, email)
+	if err != nil {
+		return false, err
+	}
+
+	checkPassword, err := helpers.CheckPassword(res.Password, []byte(password))
+	if err != nil {
+		return false, err
+	}
+
+	if res.Email == email && checkPassword {
+		return true, nil
+	}
+	return false, nil
+}
