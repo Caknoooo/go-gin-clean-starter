c8dc0f0d8bd6875c1c3233ed0fecfc215b052db5
Caknoooo <caknocomel@gmail.com>
2023-10-31 13:35:56 +0700

    chore: uncomment DeletedAt

--- entity/common.go
+++ entity/common.go
@@ -2,12 +2,13 @@ package entity
 
 import (
 	"time"
+	"gorm.io/gorm"
 )
 
 type Timestamp struct {
 	CreatedAt time.Time `gorm:"type:timestamp with time zone" json:"created_at"`
 	UpdatedAt time.Time `gorm:"type:timestamp with time zone" json:"updated_at"`
-	// DeletedAt gorm.DeletedAt
+	DeletedAt gorm.DeletedAt
 }
 
 type Authorization struct {
