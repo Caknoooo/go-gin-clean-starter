fefd515ab98d295920e1391fc5792c3415a319f8
EngenMe <mohamdfarouk727@gmail.com>
2025-05-04 21:05:44 +0100

    Remove unused vars

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -9,20 +9,18 @@ import (
 
 const (
 	// Failed
-	MESSAGE_FAILED_GET_DATA_FROM_BODY      = "failed get data from body"
-	MESSAGE_FAILED_REGISTER_USER           = "failed create user"
-	MESSAGE_FAILED_GET_LIST_USER           = "failed get list user"
-	MESSAGE_FAILED_GET_USER_TOKEN          = "failed get user token"
-	MESSAGE_FAILED_TOKEN_NOT_VALID         = "token not valid"
-	MESSAGE_FAILED_TOKEN_NOT_FOUND         = "token not found"
-	MESSAGE_FAILED_GET_USER                = "failed get user"
-	MESSAGE_FAILED_LOGIN                   = "failed login"
-	MESSAGE_FAILED_WRONG_EMAIL_OR_PASSWORD = "wrong email or password"
-	MESSAGE_FAILED_UPDATE_USER             = "failed update user"
-	MESSAGE_FAILED_DELETE_USER             = "failed delete user"
-	MESSAGE_FAILED_PROSES_REQUEST          = "failed proses request"
-	MESSAGE_FAILED_DENIED_ACCESS           = "denied access"
-	MESSAGE_FAILED_VERIFY_EMAIL            = "failed verify email"
+	MESSAGE_FAILED_GET_DATA_FROM_BODY = "failed get data from body"
+	MESSAGE_FAILED_REGISTER_USER      = "failed create user"
+	MESSAGE_FAILED_GET_LIST_USER      = "failed get list user"
+	MESSAGE_FAILED_TOKEN_NOT_VALID    = "token not valid"
+	MESSAGE_FAILED_TOKEN_NOT_FOUND    = "token not found"
+	MESSAGE_FAILED_GET_USER           = "failed get user"
+	MESSAGE_FAILED_LOGIN              = "failed login"
+	MESSAGE_FAILED_UPDATE_USER        = "failed update user"
+	MESSAGE_FAILED_DELETE_USER        = "failed delete user"
+	MESSAGE_FAILED_PROSES_REQUEST     = "failed proses request"
+	MESSAGE_FAILED_DENIED_ACCESS      = "denied access"
+	MESSAGE_FAILED_VERIFY_EMAIL       = "failed verify email"
 
 	// Success
 	MESSAGE_SUCCESS_REGISTER_USER           = "success create user"
@@ -37,18 +35,13 @@ const (
 
 var (
 	ErrCreateUser             = errors.New("failed to create user")
-	ErrGetAllUser             = errors.New("failed to get all user")
 	ErrGetUserById            = errors.New("failed to get user by id")
 	ErrGetUserByEmail         = errors.New("failed to get user by email")
 	ErrEmailAlreadyExists     = errors.New("email already exist")
 	ErrUpdateUser             = errors.New("failed to update user")
-	ErrUserNotAdmin           = errors.New("user not admin")
 	ErrUserNotFound           = errors.New("user not found")
 	ErrEmailNotFound          = errors.New("email not found")
 	ErrDeleteUser             = errors.New("failed to delete user")
-	ErrPasswordNotMatch       = errors.New("password not match")
-	ErrEmailOrPassword        = errors.New("wrong email or password")
-	ErrAccountNotVerified     = errors.New("account not verified")
 	ErrTokenInvalid           = errors.New("token invalid")
 	ErrTokenExpired           = errors.New("token expired")
 	ErrAccountAlreadyVerified = errors.New("account already verified")
@@ -114,15 +107,5 @@ type (
 	UserLoginRequest struct {
 		Email    string `json:"email" form:"email" binding:"required"`
 		Password string `json:"password" form:"password" binding:"required"`
-	}
-
-	UserLoginResponse struct {
-		Token string `json:"token"`
-		Role  string `json:"role"`
-	}
-
-	UpdateStatusIsVerifiedRequest struct {
-		UserId     string `json:"user_id" form:"user_id" binding:"required"`
-		IsVerified bool   `json:"is_verified" form:"is_verified"`
 	}
 )
