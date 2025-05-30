686c380dcfa1790516ffdf2e268fa6a9f4ffa3da
Caknoooo <caknocomel@gmail.com>
2025-05-30 11:12:30 +0700

    fix: remove unused deleteAt in timestamp

--- entity/common.go
+++ entity/common.go
@@ -1,14 +1,12 @@
 package entity
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
 type Timestamp struct {
 	CreatedAt time.Time `gorm:"type:timestamp with time zone" json:"created_at"`
 	UpdatedAt time.Time `gorm:"type:timestamp with time zone" json:"updated_at"`
-	DeletedAt gorm.DeletedAt
 }
 
 type Authorization struct {
