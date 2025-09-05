a483511343c39744d57a171396faffee264168f0
Caknoooo <caknocomel@gmail.com>
2025-09-05 12:57:25 +0700

    feat: remove rendundant dependency injection

--- providers/core.go
+++ providers/core.go
@@ -2,7 +2,11 @@ package providers
 
 import (
 	"github.com/Caknoooo/go-gin-clean-starter/config"
+	authRepo "github.com/Caknoooo/go-gin-clean-starter/modules/auth/repository"
+	userService "github.com/Caknoooo/go-gin-clean-starter/modules/user/service"
 	"github.com/Caknoooo/go-gin-clean-starter/modules/auth/service"
+	"github.com/Caknoooo/go-gin-clean-starter/modules/user/controller"
+	"github.com/Caknoooo/go-gin-clean-starter/modules/user/repository"
 	"github.com/Caknoooo/go-gin-clean-starter/pkg/constants"
 	"github.com/samber/do"
 	"gorm.io/gorm"
@@ -25,6 +29,17 @@ func RegisterDependencies(injector *do.Injector) {
 	db := do.MustInvokeNamed[*gorm.DB](injector, constants.DB)
 	jwtService := do.MustInvokeNamed[service.JWTService](injector, constants.JWTService)
 
-	// Provide Dependencies
-	ProvideUserDependencies(injector, db, jwtService)
+	// Repository
+	userRepository := repository.NewUserRepository(db)
+	refreshTokenRepository := authRepo.NewRefreshTokenRepository(db)
+
+	// Service
+	userService := userService.NewUserService(userRepository, refreshTokenRepository, jwtService, db)
+
+	// Controller
+	do.Provide(
+		injector, func(i *do.Injector) (controller.UserController, error) {
+			return controller.NewUserController(i, userService), nil
+		},
+	)
 }
